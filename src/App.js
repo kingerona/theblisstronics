094231427432386b051fc41d13059d655c22ac54
Arifur Rahman <arif037@pop-os.localdomain>
2022-07-21 17:38:46 +0600

    1st change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Deployment in progress..
+        Deployment in progress.. testing
       </header>
     </div>
   );
